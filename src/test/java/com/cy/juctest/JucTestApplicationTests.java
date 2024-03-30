0b6272fa7d933d4f1802f78455ada3852241575b
CY <chenchuanyou912@163.com>
2024-03-30 21:21:14 +0800

    juc-test工程创建

---
+++ src/test/java/com/cy/juctest/JucTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cy.juctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JucTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
